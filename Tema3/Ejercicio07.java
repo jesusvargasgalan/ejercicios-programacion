5826ddf7b549fece62897ac5a9ec186314222a53
Jesus Vargas Galan <jesusvargasgalan1@gmail.com>
2017-10-09 17:35:55 +0200

    Ejercicio07

---
+++ Tema3/Ejercicio07.java
@@ -0,0 +1,36 @@
+/*
+ * Ejercicio07.java
+ * 
+ * Jesus Vargas Galan
+ * 
+ *Escribe un programa que calcule el total de una factura a partir de la base
+ *imponible.
+ * 
+ * 
+ */
+
+
+import java.util.Scanner ;
+
+public class Ejercicio07 {
+
+  public static void main (String args[]) {
+    
+    
+    Scanner teclado = new Scanner(System.in);
+    
+    System.out.println("Introduce el precio de tu factura");
+    double factura = teclado.nextDouble();
+    
+    
+    System.out.println("Introduce la base imponible");
+    double baseImponible = teclado.nextDouble();
+    
+    
+    double facturatotal=factura % baseImponible;
+    
+    
+    System.out.println("El precio a pagar sera de "+(facturatotal+factura)+" €");
+	}
+}
+
