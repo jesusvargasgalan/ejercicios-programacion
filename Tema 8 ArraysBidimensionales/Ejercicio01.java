119c349b14e0731d438d2c451574074ab881d226
Jesus Vargas Galan <jesusvargasgalan1@gmail.com>
2017-12-11 09:35:23 +0100

    Tema 8

---
+++ Tema 8 ArraysBidimensionales/Ejercicio01.java
@@ -0,0 +1,39 @@
+
+public class Ejercicio01 {
+
+  public static void main(String[] args) {
+
+    int[][] num = new int[3][6];
+    int f = 0;
+    int c = 0;
+
+    num[0][0] = 0;
+    num[0][1] = 30;
+    num[0][2] = 2;
+    num[0][5] = 5;
+    num[1][0] = 75;
+    num[1][4] = 0;
+    num[2][2] = -2;
+    num[2][3] = 9;
+    num[2][5] = 11;
+
+    for (f = 0; f < 3; f++) {
+      System.out.print("Fila " + f + " ");
+      System.out.println();
+      for (c = 0; c < 6; c++) {
+        System.out.print("Columna " + c + " = ");
+        System.out.print(num[f][c] + " ");
+
+
+
+      }
+      System.out.println();
+
+
+
+    }
+
+
+
+  }
+}
---
+++ Tema 8 ArraysBidimensionales/Ejercicio02.java
@@ -0,0 +1,47 @@
+
+public class Ejercicio02 {
+
+  public static void main(String[] args) {
+
+    int[][] numeros = new int[4][5];
+    int f = 0;
+    int c = 0;
+    int sumaFilas = 0;
+    int auxFilas = 0;
+    int auxColumnas = 0;
+
+    for (f = 0; f < 4; f++) {
+      for (c = 0; c < 5; c++) {
+        int numeroAleatorio = (int) (Math.random() * 21);
+        numeros[f][c] = numeroAleatorio;
+        System.out.printf("%7d ", numeros[f][c]);
+        sumaFilas += numeros[f][c];
+
+      }
+
+
+      System.out.printf("|%7d\n", sumaFilas);
+    }
+    for (c = 0; c < 5; c++) {
+      System.out.print("----------");
+    }
+    System.out.println("-----------");
+
+    int sumaColumna;
+    int sumaTotal = 0;
+    for (c = 0; c < 5; c++) {
+      sumaColumna = 0;
+      for (f = 0; f < 4; f++) {
+        sumaColumna += numeros[f][c];
+      }
+
+      sumaTotal += sumaColumna;
+      System.out.printf("%7d   ", sumaColumna);
+    }
+    System.out.printf("|%7d   ", sumaTotal);
+  }
+
+
+
+}
+
